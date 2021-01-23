bb01e42af37e1a3b4d748f467c9d20ee2e7f01aa
promin3737 <s.s.hayabusa3737@icloud.com>
2021-01-23 16:42:35 +0900

    JSONの使い方

---
+++ 1_workspace/Week3/main.js
@@ -0,0 +1,36 @@
+const inputElement = document.getElementById("input")
+const addButton = document.getElementById("addButton")
+const container = document.getElementById("container")
+
+let list = []
+//localstrorageからオブジェクトに変換
+if(localStorage["list"]){
+  list = JSON.parse(localStorage["list"])
+}
+
+
+//配列listの要素の数に合わせて保存できるようにする
+for(let i = 0; i < list.length; i++){
+  //cardという箱を作って
+  const card = document.createElement("div")
+  //箱の中身を入れる
+  card.textContent = list[i]
+  //containerにcardを入れる
+  container.append(card)
+}
+
+
+addButton.onclick = function(){
+  const card = document.createElement("div")
+  card.textContent = inputElement.value
+  container.append(card)
+
+  list.push(inputElement.value)
+  localStorage.list = JSON.stringify(list)
+
+  console.log(card)
+  
+  inputElement.value = ''
+}
+
+//localStorage["text"] = "こんにちは"
