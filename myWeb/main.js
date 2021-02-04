d90f747bc1373044019be37464f6828b3ab91896
promin3737 <s.s.hayabusa3737@icloud.com>
2021-02-04 13:29:24 +0900

    [Finish] myWebsite

--- myWeb/main.js
+++ myWeb/main.js
@@ -1,24 +1,26 @@
 //スライドショー で表示させる画像パスを変数に格納
-var setImage = [
+const setImage = [
   "tourou_JPG.jpg",
+  "shrine_JPG.jpg",
   "baseball_JPG.jpg",
+  "Eco-Habitat関西学院_JPG.jpg",
   "tourou_JPG.jpg"];
 
 //カウンターの初期値設定
-var counter = 0;
+let counter = 0;
 
 //設定した画像パスをid="slideshow"のbackground-imageに設定する、繰り返す
 function slideimage(){
     //カウンターが設定画像数より大きいか確認
-    if( counter >= (setImage.length-1)){
+    if(counter >= (setImage.length-1)){
         //カウンターを初期値に戻す
         counter = 0;
     }
     //背景画像を設定
-    document.getElementById( "slideshow" ).style.backgroundImage = "url(" + setImage[counter] + ")";
+    document.getElementById("slideshow").style.backgroundImage = "url(" + setImage[counter] + ")";
     //カウントアップ
     counter++;
 }
 
-//3秒ごとに背景画像を切り替える
+//5秒ごとに背景画像を切り替える
 setInterval("slideimage()",5000);
